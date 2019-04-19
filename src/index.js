65ba618ef8916cd0e7f396dd55b392a73a0376b4
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-04-19 12:16:59 +0200

    Add mongoDB via Atlas cloud service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let config = require('dotenv').config().parsed;
+
+// prettier-ignore
+const url = `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@${config.MONGODB_SERVER}/${config.DB_NAME}`;
+
+mongoose.connect(url, { useNewUrlParser: true, retryWrites: true });
+mongoose.connection.once('open', () =>
+  console.log(`Connected to mongo at ${url}`),
+);
 
 // Set up Apollo Server
 const server = new ApolloServer({
