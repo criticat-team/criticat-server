600177bcb0a8ffd781a06adfe64176fd094153d2
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2019-08-08 17:13:49 +0200

    Allow getting all items (without a category)

--- src/resolvers.js
+++ src/resolvers.js
@@ -7,9 +7,10 @@ const entities = new AllHtmlEntities();
 export default {
   Query: {
     books: () => Book.find({}),
-    articles: async (root, { category, itemsPerPage, continuation }, { dataSources }) => {
-      return dataSources.articlesAPI.getCategory(category, itemsPerPage, continuation);
-    },
+    articles: async (root, { category, itemsPerPage, continuation }, { dataSources }) =>
+      category
+        ? dataSources.articlesAPI.getCategory(itemsPerPage, continuation, category)
+        : dataSources.articlesAPI.getAll(itemsPerPage, continuation),
   },
   Article: {
     image: article =>
@@ -26,6 +27,16 @@ export default {
     origin: async (article, args, { dataSources }) => {
       const { subscriptions } = await dataSources.articlesAPI.getSubscriptions();
       return subscriptions.find(subscription => subscription.id === article.origin.streamId);
+    },
+    categories: article => {
+      const labelCategoryRegex = new RegExp(`user/${process.env.INOREADER_USER_ID}/label/(.*)`);
+      return article.categories.reduce((categories, category) => {
+        const match = category.match(labelCategoryRegex);
+        if (match) {
+          categories.push(match[1]);
+        }
+        return categories;
+      }, []);
     },
   },
 };
